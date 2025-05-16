6199a77c7ea090aba397d58d0d2f2795240fc10c
wocjs <friendly1217@naver.com>
2025-05-16 14:27:40 +0900

    backend: auth middleware

---
+++ miniGram/server/middleware/auth.js
@@ -0,0 +1,14 @@
+import { verify } from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: '인증 필요' });
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch {
+    res.status(403).json({ message: '토큰 오류' });
+  }
+};
