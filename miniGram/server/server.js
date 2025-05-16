f0e2f44fe3785d849640758d237d3c3e64bc7b38
wocjs <friendly1217@naver.com>
2025-05-16 14:51:14 +0900

    Add routes

--- miniGram/server/server.js
+++ miniGram/server/server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const db = require('./db/db');
+import express from 'express';
+import cors from 'cors';
+import db from './db/db';
+import likesRouter from './routes/likes.js';
+import hashtagsRouter from './routes/hashtags.js';
+import reviewsRouter from './routes/reviews.js';
+import reviewLikesRouter from './routes/reviewLikes.js';
 
 const app = express();
 app.use(cors());
@@ -10,6 +14,10 @@ app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
+app.use('/api/likes', likesRouter);
+app.use('/api/hashtags', hashtagsRouter);
+app.use('/api/reviews', reviewsRouter);
+app.use('/api/review-likes', reviewLikesRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`서버 실행 중: http://localhost:${PORT}`));
